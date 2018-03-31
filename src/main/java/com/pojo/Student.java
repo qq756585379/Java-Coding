f1ccb8d97e6d46949c4280271a774a0137694317
杨俊 <756585379@qq.com>
2018-03-31 17:15:01 +0800

    cglib测试

---
+++ src/main/java/com/pojo/Student.java
@@ -0,0 +1,23 @@
+package com.pojo;
+
+public class Student {
+
+    private String name = "张三";
+    private String rename = "我是rename";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRename() {
+        return rename;
+    }
+
+    public void setRename(String rename) {
+        this.rename = rename;
+    }
+}
